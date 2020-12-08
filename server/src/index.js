ddab458d24bef27a9401acd6ded122177ab26f93
Brendan-Fisher <btfisher1229@gmail.com>
2020-12-08 03:36:38 -0500

    fixed path to build folder

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,11 @@
 const app = require("./app");
 const path = require("path")
 
+
 const port = process.env.PORT || 9000;
 
 if(process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static('../../client/build'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client','build','index.html'))
