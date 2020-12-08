b60878787e46c20deca9f195ddd6a80688051055
Brendan-Fisher <btfisher1229@gmail.com>
2020-12-07 22:56:42 -0500

    added new icon for the stops shared by both paths

---
+++ client/src/icons/icons.js
@@ -0,0 +1,49 @@
+import stop_icon from './stop_icon.svg'
+import djikstra_icon from './djikstra.svg'
+import bfs_icon from './bfs.svg'
+import startPin from './start.svg'
+import endPin from './end.svg'
+import both_icon from './both.svg'
+import L from "leaflet";
+
+export var stopIcon = L.icon({
+    iconUrl: stop_icon,
+    iconSize: [30, 30],
+    iconAnchor: [15, 30],
+    popupAnchor: [0, -30],
+});
+
+export var both = L.icon({
+    iconUrl: both_icon,
+    iconSize: [30, 30],
+    iconAnchor: [15, 30],
+    popupAnchor: [0, -30],
+})
+
+export var startIcon = L.icon({
+    iconUrl: startPin,
+    iconSize: [50, 50],
+    iconAnchor: [25, 50],
+    popupAnchor: [0, -50],
+})
+
+export var finishIcon = L.icon({
+    iconUrl: endPin,
+    iconSize: [50, 50],
+    iconAnchor: [25, 50],
+    popupAnchor: [0, -50],
+})
+
+export var DIcon = L.icon({
+    iconUrl: djikstra_icon,
+    iconSize: [30, 30],
+    iconAnchor: [15, 30],
+    popupAnchor: [0, -30],
+});
+
+export var BIcon = L.icon({
+    iconUrl: bfs_icon,
+    iconSize: [30, 30],
+    iconAnchor: [15, 30],
+    popupAnchor: [0, -30],
+});
